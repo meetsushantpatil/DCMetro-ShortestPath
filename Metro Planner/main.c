060098ce69922898c562535d54fd77a7250476fa
Sushant Patil <Sushant@sushants-macbook-pro.local>
2015-12-06 22:48:42 -0500

    Initial Commit

---
+++ Metro Planner/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Metro Planner
+//
+//  Created by Sushant Patil on 06/12/15.
+//  Copyright © 2015 Sushant Patil. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
